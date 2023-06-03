050000423b198e1e68b59be7d3a6664c9e028157
Wang Xuxin <93122402+WXUXIN@users.noreply.github.com>
2023-06-03 12:59:24 +0800

    sda

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import {useState} from 'react';
-import {useEffect} from 'react';
+// import {useEffect} from 'react';
 // import { Button } from "../../../dropdown-menu"
 // import {
 //   DropdownMenu,
