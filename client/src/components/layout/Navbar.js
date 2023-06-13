afb4308be418ea4365c41bc52d5a70c956623522
Marcus Kim <kimmarcus2@gmail.com>
2023-06-13 15:43:11 +0800

    rating test

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = ({ auth: { isAuthenticated }, logout }) => {
     <ul>
       <li className = "navbar-register-login"><Link to='/register'>Register</Link></li>
       <li className = "navbar-register-login"><Link to="/login">Login</Link></li>
-      <li className = "navbar-register-login"><Link to="/rate-tutor">Login</Link></li>
+      <li className = "navbar-register-login"><Link to="/rate-tutor">rate tutor</Link></li>
     </ul>
     );
   
