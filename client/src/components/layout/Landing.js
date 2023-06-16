bea7dd9ed2ac83bc2b1f59b554e085fb5360aacd
Marcus Kim <kimmarcus2@gmail.com>
2023-06-16 20:24:35 +0800

    final landing page 1

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -109,45 +109,69 @@ export default function Landing() {
         <div className="background-image-container"></div>
 
           <div className="landing-inner">
+          {/* {all below in landing-inner} */}
 
-            {/* {all below in landing-inner} */}
-            <h1 className="x-large-landing">
-              I am in &nbsp;&nbsp;   
-          
-            <select value={levelOfStudy} 
-              onChange={handleLevelOfStudyChange} 
-              className='dropdown'
-              style={{ fontSize: 'inherit', backgroundColor: 'grey', color: '#e9c78c'}}>
-                <option value="">Level of Study</option>
-                <option value="Primary School">Primary School</option>
-                <option value="Secondary School">Secondary School</option>
-                <option value="Junior College">Junior College</option>
-              </select>
-              </h1>
-          
-          
+            <div className="box-container" style={{ backgroundColor: 'grey',
+                padding: '100px 20px 100px',
+                opacity: '1',
+                borderRadius: '30px',
+                position: 'relative'}}>
 
-            <h1 className="x-large-landing" style={{ marginTop: '50px' }}>
-                I am looking for a &nbsp;&nbsp;
-                <select value={subject} 
-                onChange={handleSubjectChange} 
-                className="dropdown"
-                style={{ fontSize: 'inherit', backgroundColor: 'grey', color: '#e9c78c' }}>
-                  {subjectOptions.length === 0 ? (
-                    <option value="">Select level of study</option>
-                  ) : (
-                    <>
-                      <option value="">Select subject</option>
-                      {subjectOptions.map((option) => (
-                        <option key={option} value={option}>
-                          {option}
-                        </option>
-                      ))}
-                    </>
-                  )}
+              <h1 className="x-large-landing">
+                I am in &nbsp;&nbsp;   
+            
+              <select value={levelOfStudy} 
+                onChange={handleLevelOfStudyChange} 
+                className='dropdown'
+                style={{ fontSize: 'inherit', backgroundColor: 'grey', color: '#e9c78c',  borderRadius: '30px', textAlign: 'center', padding: '8px'}}>
+                  <option value="">Level of Study</option>
+                  <option value="Primary School">Primary School</option>
+                  <option value="Secondary School">Secondary School</option>
+                  <option value="Junior College">Junior College</option>
                 </select>
-                &nbsp; tutor.
-              </h1>
+                </h1>
+            
+            
+              <h1 className="x-large-landing" style={{ marginTop: '30px' }}>
+                  I am looking for a &nbsp;&nbsp;
+                  <select value={subject} 
+                  onChange={handleSubjectChange} 
+                  className="dropdown"
+                  style={{ fontSize: 'inherit', backgroundColor: 'grey', color: '#e9c78c', borderRadius: '30px', textAlign: 'center' , padding: '8px'}}>
+                    {subjectOptions.length === 0 ? (
+                      <option value="">Select level of study</option>
+                    ) : (
+                      <>
+                        <option value="">Select subject</option>
+                        {subjectOptions.map((option) => (
+                          <option key={option} value={option}>
+                            {option}
+                          </option>
+                        ))}
+                      </>
+                    )}
+                  </select>
+                  &nbsp; tutor.
+                </h1>
+                <div>
+                <button style={{
+                    fontFamily: 'Josefin Sans',
+                    alignSelf: 'flex-start',
+                    backgroundColor: '#0B1B3D',
+                    color: 'white',
+                    borderRadius: '5px',
+                    padding: '20px 30px',
+                    border: 'none',
+                    outline: 'none',
+                    cursor: 'pointer',
+                    position: 'absolute',
+                    bottom: '20px',
+                    right: '20px',
+                    fontSize: '25px'
+                  }}>Find my tutor</button>
+                </div>
+
+              </div>
           </div>
         
       </section>
