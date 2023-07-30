2adcaa2c7e48cd7533f075267f72af434da2dbc5
Marcus Kim <kimmarcus2@gmail.com>
2023-07-30 10:44:20 +0800

    tutordashboard test

--- client/src/components/auth/Login.test.js
+++ client/src/components/auth/Login.test.js
@@ -1,18 +1,15 @@
-// Login.test.js
-
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store'; // You may need to install this package
+import configureStore from 'redux-mock-store';
+import { MemoryRouter, Route, Redirect } from 'react-router-dom';
 
 import Login from './Login';
 
-// Create a mock store for testing
 const mockStore = configureStore([]);
 
 describe('Login Component', () => {
   it('should render the login form', () => {
-    // Create a mock store with any initial state needed
     const store = mockStore({
       auth: {
         isAuthenticated: false,
@@ -20,18 +17,67 @@ describe('Login Component', () => {
       },
     });
 
-    // Render the Login component with the mock store as a Provider
     const { getByPlaceholderText, getByText } = render(
       <Provider store={store}>
-        <Login />
+        <MemoryRouter initialEntries={['/login']}>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </MemoryRouter>
       </Provider>
     );
 
-    // Check if the login form elements are rendered correctly
     expect(getByPlaceholderText('Email Address')).toBeInTheDocument();
     expect(getByPlaceholderText('Password')).toBeInTheDocument();
     expect(getByText('Login')).toBeInTheDocument();
+    expect(getByText("Don't have an account?")).toBeInTheDocument();
   });
 
-  // You can add more test cases to cover other scenarios like form submissions, error handling, etc.
+  it('should redirect to TutorDashboard if user is authenticated and isTutor is true', () => {
+    const store = mockStore({
+      auth: {
+        isAuthenticated: true,
+        user: { isTutor: true },
+      },
+    });
+
+    const { container } = render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/login']}>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/TutorDashboard">Tutor Dashboard</Route>
+          {/* Use Redirect component to handle redirection */}
+          <Redirect to="/TutorDashboard" />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(container.textContent).toContain('Tutor Dashboard');
+  });
+
+  it('should redirect to /profiles if user is authenticated and isTutor is false', () => {
+    const store = mockStore({
+      auth: {
+        isAuthenticated: true,
+        user: { isTutor: false },
+      },
+    });
+
+    const { container } = render(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/login']}>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/profiles">Profiles</Route>
+          {/* Use Redirect component to handle redirection */}
+          <Redirect to="/profiles" />
+        </MemoryRouter>
+      </Provider>
+    );
+
+    expect(container.textContent).toContain('Profiles');
+  });
 });
